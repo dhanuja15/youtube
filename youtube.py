cfe6e1e1c4eadbc734f6c5f1ab799186776fa50a
Dhanuja K <dhanuja1503@gmail.com>
2024-04-12 21:25:49 +0530

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -2,15 +2,15 @@
 from googleapiclient.discovery import build
 import pandas as pd
 import datetime
-import isodate
 import plotly.graph_objects as go
 import plotly.express as px
+import isodate
 import mysql.connector as db
 import streamlit as st
 
 #API Key connection
 def connect_api():
-    api_id="ENTER YOUR API KEY"
+    api_id="AIzaSyD9moyK4kbvtjLzFbIg8zqJqvX8tP1D33s"
     api_service_name="youtube"
     api_version="v3"
     youtube=build(api_service_name,api_version,developerKey=api_id)
@@ -20,29 +20,33 @@ youtube=connect_api()
 #Fetch channel details
 
 def get_channel_info(channel_id):
-    channel_data=[]
-    request = youtube.channels().list(
-        part="snippet,contentDetails,statistics",
-        id=channel_id)
+    try:
+        channel_data=[]
+        request = youtube.channels().list(
+            part="snippet,contentDetails,statistics",
+            id=channel_id)
+        response = request.execute()
+        for item in response.get("items",[]):
+            data = {
+                "channel_name": item["snippet"]["title"],
+                "channel_id": item["id"],
+                "channel_views": item["statistics"]["viewCount"],
+                "channel_description": item["snippet"]["description"],
+                "subscribers": item["statistics"]["subscriberCount"],
+                "total_videos": item["statistics"]["videoCount"],
+                "playlist_id": item["contentDetails"]["relatedPlaylists"]["uploads"]
+            }
+            channel_data.append(data)
+        return channel_data                
+    except:
+        pass
     
-    response = request.execute()
-    for item in response.get("items",[]):
-        data = dict(channel_name= item["snippet"]["title"],
-                    channel_id= item["id"],
-                    channel_views= item["statistics"]["viewCount"],
-                    channel_description= item["snippet"]["description"],
-                    subscribers=item["statistics"]["subscriberCount"],
-                    total_videos= item["statistics"]["videoCount"],
-                    playlist_id= item["contentDetails"]["relatedPlaylists"]["uploads"])
-        channel_data.append(data)       
-    return channel_data
-
 
 # Fetch playlist details
 def get_playlist_details(channel_id):
     try:
-        next_page_token = None
         playlists=[]
+        next_page_token = None
         while True:
             request = youtube.playlists().list(
                 part="snippet,contentDetails",
@@ -65,6 +69,7 @@ def get_playlist_details(channel_id):
     except :
         pass
     return playlists
+
 
  #Fetch video IDs
 def get_video_ids(channel_id):
@@ -150,9 +155,9 @@ def get_comment_info(channel_id):
 def channel_tb(channel_id):
     
     mydb = db.connect(
-        host='enter your hostname',
-        user=' enter your user name',
-        password='enter your password',
+        host='localhost',
+        user='root',
+        password='root',
         database='youtube')
     cursor = mydb.cursor()
     
@@ -172,9 +177,9 @@ def channel_tb(channel_id):
 def playlist_tb(channel_id):
 
     mydb = db.connect(
-        host='enter your hostname',
-        user=' enter your user name',
-        password='enter your password',
+        host='localhost',
+        user='root',
+        password='root',
         database='youtube')
     cursor = mydb.cursor()
     try:
@@ -193,9 +198,9 @@ def playlist_tb(channel_id):
 #video table
 def video_tb(channel_id):
     mydb = db.connect(
-        host='enter your hostname',
-        user=' enter your user name',
-        password='enter your password',
+        host='localhost',
+        user='root',
+        password='root',
         database='youtube')
     cursor = mydb.cursor()
     
@@ -211,7 +216,7 @@ def video_tb(channel_id):
                         like_count INT,\
                         favorite_count INT,\
                         comment_count INT,\
-                        duration TIME,\
+                        duration varchar(25),\
                         thumbnail VARCHAR(255))")
         video_info=get_video_info(channel_id)
         video_data=pd.DataFrame(video_info)
@@ -228,9 +233,9 @@ def video_tb(channel_id):
 #comment table
 def comment_tb(channel_id):
     mydb = db.connect(
-        host='enter your hostname',
-        user=' enter your user name',
-        password='enter your password',
+        host='localhost',
+        user='root',
+        password='root',
         database='youtube')
     cursor = mydb.cursor()
     try:
@@ -306,9 +311,9 @@ if st.button("Store data to MySQL DB"):
 
 #MySQL connection
 mydb = db.connect(
-        host='enter your hostname',
-        user=' enter your user name',
-        password='enter your password',
+        host='localhost',
+        user='root',
+        password='root',
         database='youtube')
 cursor = mydb.cursor()
 
@@ -320,101 +325,107 @@ query_option = st.selectbox('Select SQL Query Option',
                              '5.Videos with Highest Likes', '6.Likes by Video',
                              '7.Total Views by Channel', '8.Channels with Videos in 2022',
                              '9.Average Video Duration by Channel', '10.Videos with Most Comments'])
+if st.button("View"):
 
-# Execute SQL Query based on user selection
-if query_option == '1.Video and Channel Names':
+    # Execute SQL Query based on user selection
+    if query_option == '1.Video and Channel Names':
+            
+        query1 = "SELECT video_name, channel_name FROM video"
+        cursor.execute(query1)
+        c1=cursor.fetchall()
+        df1=pd.DataFrame(c1,columns=["Video Title","Channel Name"])
+        st.write(df1)
+
+    elif query_option == '2.Channels with Most Videos':
         
-    query1 = "SELECT video_name, channel_name FROM video"
-    cursor.execute(query1)
-    c1=cursor.fetchall()
-    df1=pd.DataFrame(c1,columns=["Video Title","Channel Name"])
-    st.write(df1)
-
-elif query_option == '2.Channels with Most Videos':
-    
-    query2 = "SELECT channel_name, total_videos AS video_count FROM channels  ORDER BY video_count DESC"
-    cursor.execute(query2)
-    c2=cursor.fetchall()
-    df2=pd.DataFrame(c2,columns=["Channel Name","No.of Videos"])
-    st.write(df2)
-    fig2 = go.Figure(data=[go.Bar(x=df2['No.of Videos'], y=df2['Channel Name'], orientation='h')])
-    fig2.update_layout(title='Channels with Most Videos', xaxis_title='Number of Videos', yaxis_title='Channel Name')
-    st.plotly_chart(fig2)
+        query2 = "SELECT channel_name, total_videos AS video_count FROM channels  ORDER BY video_count DESC"
+        cursor.execute(query2)
+        c2=cursor.fetchall()
+        df2=pd.DataFrame(c2,columns=["Channel Name","No.of Videos"])
+        st.write(df2)
+        fig2 = go.Figure(data=[go.Bar(x=df2['No.of Videos'], y=df2['Channel Name'], orientation='h')])
+        fig2.update_layout(title='Channels with Most Videos', xaxis_title='Number of Videos', yaxis_title='Channel Name')
+        st.plotly_chart(fig2)
 
 
-elif query_option == '3.Top 10 Most Viewed Videos':
-    
-    query3 = "SELECT view_count, channel_name, video_name  FROM video where view_count is not null ORDER BY view_count DESC LIMIT 10"
-    cursor.execute(query3)
-    c3=cursor.fetchall()
-    df3=pd.DataFrame(c3,columns=["Views","Channel Name","Video Title"])
-    st.write(df3)
-    fig3 = go.Figure(data=[go.Bar(y=df3['Video Title'], x=df3['Views'], orientation='h')])
-    fig3.update_layout(title='Top 10 Most Viewed Videos', xaxis_title='Views', yaxis_title='Video Title')
-    st.plotly_chart(fig3)
+    elif query_option == '3.Top 10 Most Viewed Videos':
+        
+        query3 = "SELECT view_count, channel_name, video_name  FROM video where view_count is not null ORDER BY view_count DESC LIMIT 10"
+        cursor.execute(query3)
+        c3=cursor.fetchall()
+        df3=pd.DataFrame(c3,columns=["Views","Channel Name","Video Title"])
+        st.write(df3)
+        fig3 = go.Figure(data=[go.Bar(y=df3['Video Title'], x=df3['Views'], orientation='h')])
+        fig3.update_layout(title='Top 10 Most Viewed Videos', xaxis_title='Views', yaxis_title='Video Title')
+        st.plotly_chart(fig3)
 
-elif query_option == '4.Comments Count by Video':
-    
-    query4 = "SELECT comment_count, video_name FROM video where comment_count is not null"
-    cursor.execute(query4)
-    c4=cursor.fetchall()
-    df4=pd.DataFrame(c4,columns=["No.of Comments","Video Title"])
-    st.write(df4)
-    
-elif query_option == '5.Videos with Highest Likes':
-    
-    query5 = "SELECT video_name, channel_name, like_count FROM video where like_count is not null ORDER BY like_count DESC"
-    cursor.execute(query5)
-    c5=cursor.fetchall()
-    df5=pd.DataFrame(c5,columns=["Video Title","Channel Name","Like Count"])
-    st.write(df5)
-    
-elif query_option == '6.Likes by Video':
-    
-    query6 = "select video_name, like_count from video where like_count is not null"
-    cursor.execute(query6)
-    c6=cursor.fetchall()
-    df6=pd.DataFrame(c6,columns=["Videos Title","Likes Count"])
-    st.write(df6)
-    
+    elif query_option == '4.Comments Count by Video':
+        
+        query4 = "SELECT comment_count, video_name FROM video where comment_count is not null"
+        cursor.execute(query4)
+        c4=cursor.fetchall()
+        df4=pd.DataFrame(c4,columns=["No.of Comments","Video Title"])
+        st.write(df4)
+        
+    elif query_option == '5.Videos with Highest Likes':
+        
+        query5 = "SELECT video_name, channel_name, like_count FROM video where like_count is not null ORDER BY like_count DESC"
+        cursor.execute(query5)
+        c5=cursor.fetchall()
+        df5=pd.DataFrame(c5,columns=["Video Title","Channel Name","Like Count"])
+        st.write(df5)
+        
+    elif query_option == '6.Likes by Video':
+        
+        query6 = "select video_name, like_count from video where like_count is not null"
+        cursor.execute(query6)
+        c6=cursor.fetchall()
+        df6=pd.DataFrame(c6,columns=["Videos Title","Likes Count"])
+        st.write(df6)
+        
 
-elif query_option == '7.Total Views by Channel':
-    
-    query7 = "SELECT channel_name, channel_views FROM channels"
-    cursor.execute(query7)
-    c7=cursor.fetchall()
-    df7=pd.DataFrame(c7,columns=["Channel name","Total views"])
-    st.write(df7)
-    fig7 = go.Figure(data=[go.Bar(x=df7['Channel name'], y=df7['Total views'])])
-    fig7.update_layout(title='Total Views by Channel', xaxis_title='Channel Name', yaxis_title='Total Views')
-    st.plotly_chart(fig7)
+    elif query_option == '7.Total Views by Channel':
+        
+        query7 = "SELECT channel_name, channel_views FROM channels"
+        cursor.execute(query7)
+        c7=cursor.fetchall()
+        df7=pd.DataFrame(c7,columns=["Channel name","Total views"])
+        st.write(df7)
+        fig7 = go.Figure(data=[go.Bar(x=df7['Channel name'], y=df7['Total views'])])
+        fig7.update_layout(title='Total Views by Channel', xaxis_title='Channel Name', yaxis_title='Total Views')
+        st.plotly_chart(fig7)
 
-elif query_option == '8.Channels with Videos in 2022':
-    
-    query8 = "SELECT video_name,published_date, channel_name FROM video WHERE YEAR(published_date) = 2022"
-    cursor.execute(query8)
-    c8=cursor.fetchall()
-    df8=pd.DataFrame(c8,columns=["Video Title","Published Date","Channel Name"])
-    st.write(df8)
-    fig8 = px.scatter(df8, x="Published Date", y="Channel Name")
-    fig8.update_layout(title='Channels with Videos in 2022', xaxis_title='Published Date', yaxis_title='Channel Name')
-    st.plotly_chart(fig8)
+    elif query_option == '8.Channels with Videos in 2022':
+        
+        query8 = "SELECT video_name,published_date, channel_name FROM video WHERE YEAR(published_date) = 2022"
+        cursor.execute(query8)
+        c8=cursor.fetchall()
+        df8=pd.DataFrame(c8,columns=["Video Title","Published Date","Channel Name"])
+        st.write(df8)
+        fig8 = px.scatter(df8, x="Published Date", y="Channel Name")
+        fig8.update_layout(title='Channels with Videos in 2022', xaxis_title='Published Date', yaxis_title='Channel Name')
+        st.plotly_chart(fig8)
 
-elif query_option == '9.Average Video Duration by Channel':
-    
-    query9 = "SELECT channel_name, AVG(TIME_TO_SEC(duration)) AS average_duration_in_seconds FROM video GROUP BY channel_name"
-    cursor.execute(query9)
-    c9 = cursor.fetchall()
-    df9 = pd.DataFrame(c9, columns=["Channel name", "Average Duration (in seconds)"])
-    average_durations_seconds = df9['Average Duration (in seconds)'].astype('int')
-    average_durations_timedelta = pd.to_timedelta(average_durations_seconds, unit='s')
-    df9['Average Duration'] = average_durations_timedelta
-    st.write(df9)
+    elif query_option == '9.Average Video Duration by Channel':
+        
+        query9 = "SELECT channel_name, AVG(TIME_TO_SEC(duration)) AS average_duration_in_seconds FROM video GROUP BY channel_name"
+        cursor.execute(query9)
+        c9 = cursor.fetchall()
+        df9 = pd.DataFrame(c9, columns=["Channel name", "Average Duration (in seconds)"])
+        average_durations_seconds = df9['Average Duration (in seconds)'].astype('int')
+        average_durations_timedelta = pd.to_timedelta(average_durations_seconds, unit='s')
+        df9['Average Duration'] = average_durations_timedelta
+        st.write(df9)
 
-elif query_option == '10.Videos with Most Comments':
-    
-    query10 = "SELECT video_name, channel_name, comment_count FROM video where comment_count is not null ORDER BY comment_count DESC "
-    cursor.execute(query10)
-    c10=cursor.fetchall()
-    df10=pd.DataFrame(c10,columns=["Video Title","Channel Name","Comments"])
-    st.write(df10)
+    elif query_option == '10.Videos with Most Comments':
+        
+        query10 = "SELECT video_name, channel_name, comment_count FROM video where comment_count is not null ORDER BY comment_count DESC "
+        cursor.execute(query10)
+        c10=cursor.fetchall()
+        df10=pd.DataFrame(c10,columns=["Video Title","Channel Name","Comments"])
+        st.write(df10)
+
+
+
+
+
